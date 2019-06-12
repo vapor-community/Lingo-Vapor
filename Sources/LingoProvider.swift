45dce498aafafe52430a6bb0d967f7ecad85e754
Jordane Belanger <jb@brainfinance.com>
2019-06-12 09:54:05 -0400

    Use the DirectoryConfig service instance workDir property to locate the workDirectory instead of the `detect` method to support custom working directories as is intended by the DirectoryConfig service.

--- Sources/LingoProvider.swift
+++ Sources/LingoProvider.swift
@@ -14,16 +14,18 @@ extension Container {
 public struct LingoProvider: Vapor.Provider {
     
     let defaultLocale: String
-    let rootPath: String
+    let localizationsDir: String
     
     public init(defaultLocale: String, localizationsDir: String = "Localizations") {
         self.defaultLocale = defaultLocale
-        self.rootPath = DirectoryConfig.detect().workDir + localizationsDir
+        self.localizationsDir = localizationsDir
     }
     
     public func register(_ services: inout Services) throws {
         services.register(Lingo.self) { (container) -> (Lingo) in
-            return try Lingo(rootPath: self.rootPath, defaultLocale: self.defaultLocale)
+            let dirConfig = container.make(DirectoryConfig.self)
+            let rootPath = dirConfig.workDir + self.localizationsDir
+            return try Lingo(rootPath: rootPath, defaultLocale: self.defaultLocale)
         }
     }
     
