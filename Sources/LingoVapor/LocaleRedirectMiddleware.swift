361b19214e9ae52d9c60457f014acf705a3bb959
Nathan Fallet <contact@nathanfallet.me>
2022-04-21 15:08:35 +0200

    Adding a check to be sure that the locale exists

--- Sources/LingoVapor/LocaleRedirectMiddleware.swift
+++ Sources/LingoVapor/LocaleRedirectMiddleware.swift
@@ -4,7 +4,7 @@ public final class LocaleRedirectMiddleware: Middleware {
     public init() {}
     public func respond(to request: Request, chainingTo next: Responder) -> EventLoopFuture<Response> {
         // Check if a language is passed in url
-        guard let lang = request.parameters.get("locale")
+        guard let locale = request.parameters.get("locale")
         else {
             // Redirect corresponding to specified locale
             // For example, `/home/` will redirect to `/<request.locale>/home/`
@@ -22,9 +22,15 @@ public final class LocaleRedirectMiddleware: Middleware {
             ))
         }
         
+        // Check that the locale is available, else it's a 404
+        guard locales.contains(locale)
+        else {
+            return request.eventLoop.makeFailedFuture(Abort(.notFound))
+        }
+        
         // Set request locale from url
         // For example, `/fr/home/` will display homepage in French
-        request.session.data["locale"] = locales.contains(lang) ? lang : lingo.defaultLocale
+        request.session.data["locale"] = locale
         return next.respond(to: request)
     }
 }
